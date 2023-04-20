b0e4fb078384f2270e7f39ef766ea954e62d463d
Jacob <j31c0@hotmail.com>
2023-04-20 11:17:52 -0500

    Add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import { pageLoad } from "./initial";
 import { contact } from "./contact";
 import { menu } from "./menu";
 
+const tabs = document.querySelectorAll('.tab');
+const content = document.querySelector('#content');
+
+const clearContent = () => {
+    content.replaceChildren();
+}
+
 pageLoad();
-contact();
-menu();
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        switch (tab.id) {
+            case 'home':
+                clearContent();
+                pageLoad();
+                break;
+        
+            case 'menu':
+                clearContent();
+                menu();
+                break;
+            
+            case 'contact':
+                clearContent();
+                contact();
+                break;
+
+            default:
+                clearContent();
+                pageLoad();
+                break;
+        }
+    })
+});
